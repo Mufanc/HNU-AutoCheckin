6d59366394747abb70efc9d2f4bd6e6a00750ba4
Mufanc <47652878+Mufanc@users.noreply.github.com>
2021-10-03 21:17:34 +0800

    添加更丰富的日志，便于排错

--- main.py
+++ main.py
@@ -43,10 +43,12 @@ class Checker(httpx.Client):
         logger.info('尝试打卡中...')
         for trial in range(self.configs['max_trial']):
             try:
+                logger.info('正在登录...')
                 token = self.get('https://fangkong.hnu.edu.cn/api/v1/account/getimgvcode').json()['data']['Token']
                 image_raw = self.get(f'https://fangkong.hnu.edu.cn/imagevcode?token={token}').content
                 image = cv2.imdecode(np.frombuffer(image_raw, np.uint8), cv2.IMREAD_COLOR)
                 code = guess(image)
+
                 login = self.post(
                     'https://fangkong.hnu.edu.cn/api/v1/account/login',
                     data={
@@ -57,45 +59,48 @@ class Checker(httpx.Client):
                         'Token': token
                     }
                 ).json()
-                if not login['code']:
-                    if self.configs['random']:
-                        interval = (3, 8)
-                    else:
-                        interval = (5, 5)
-                    message = self.post(
-                        'https://fangkong.hnu.edu.cn/api/v1/clockinlog/add',
-                        headers={
-                            **self.headers,
-                            **{
-                                'Cache-Control': 'no-cache',
-                                'Host': 'fangkong.hnu.edu.cn',
-                                'Origin': 'https://fangkong.hnu.edu.cn',
-                                'Pragma': 'no-cache',
-                                'Referer': 'https://fangkong.hnu.edu.cn/app/',
-                                'Sec-Fetch-Dest': 'empty',
-                                'Sec-Fetch-Mode': 'cors',
-                                'Sec-Fetch-Site': 'same-origin'
-                            }
-                        },
-                        json={
-                            'Longitude': None,
-                            'Latitude': None,
-                            'RealProvince': self.configs['address'][0],
-                            'RealCity': self.configs['address'][1],
-                            'RealCounty': self.configs['address'][2],
-                            'RealAddress': self.configs['address'][3],
-                            'BackState': 1,
-                            'MorningTemp': f'36.{randint(*interval)}',
-                            'NightTemp': f'36.{randint(*interval)}',
-                            'tripinfolist': []
+                if login['code']:  # code 不为 0 则代表登录失败
+                    logger.error(login)
+                    continue
+
+                if self.configs['random']:
+                    interval = (3, 8)
+                else:
+                    interval = (5, 5)
+                message = self.post(
+                    'https://fangkong.hnu.edu.cn/api/v1/clockinlog/add',
+                    headers={
+                        **self.headers,
+                        **{
+                            'Cache-Control': 'no-cache',
+                            'Host': 'fangkong.hnu.edu.cn',
+                            'Origin': 'https://fangkong.hnu.edu.cn',
+                            'Pragma': 'no-cache',
+                            'Referer': 'https://fangkong.hnu.edu.cn/app/',
+                            'Sec-Fetch-Dest': 'empty',
+                            'Sec-Fetch-Mode': 'cors',
+                            'Sec-Fetch-Site': 'same-origin'
                         }
-                    ).json()['msg']
-                    if message in self.configs['success_tint']:
-                        logger.info(f'服务器消息：{message}')
-                        logger.info('自动打卡成功')
-                        break
-                    else:
-                        raise RuntimeError(f'打卡失败：{message}')
+                    },
+                    json={
+                        'Longitude': None,
+                        'Latitude': None,
+                        'RealProvince': self.configs['address'][0],
+                        'RealCity': self.configs['address'][1],
+                        'RealCounty': self.configs['address'][2],
+                        'RealAddress': self.configs['address'][3],
+                        'BackState': 1,
+                        'MorningTemp': f'36.{randint(*interval)}',
+                        'NightTemp': f'36.{randint(*interval)}',
+                        'tripinfolist': []
+                    }
+                ).json()['msg']
+                if message in self.configs['success_tint']:
+                    logger.info(f'服务器消息：{message}')
+                    logger.info('自动打卡成功！')
+                    break
+                else:
+                    raise RuntimeError(f'打卡失败：{message}')
             except Exception as err:
                 logger.error(err)
                 logger.info(f'已失败 {trial + 1} 次，将于 {self.configs["failed_wait"]} 秒后重试')
@@ -107,7 +112,7 @@ class Checker(httpx.Client):
 def main():
     configs = {
         **defaults,
-        **json.loads(os.environ['USER'])
+        **json.loads(getattr(os.environ, 'USER', '{}'))
     }
     # 检查是否配置正确
     assert 'student_id' in configs and 'password' in configs
